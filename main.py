9fc634258669ad727223babfedf114069df7172d
sharifigiv <76777409+sharifigiv@users.noreply.github.com>
2023-03-25 11:28:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import pyray as pr
 from Transform import *
 from Map import Map
 
-pr.init_window(1080, 720, "Iran Map")
+if __name__ == "__main__":
+    pr.init_window(1080, 720, "Iran Map")
 
-ir_map = Map(0, -10)
-ir_map.create_states()
+    ir_map = Map(0, -10)
+    ir_map.create_states()
 
-while not pr.window_should_close():
-    pr.begin_drawing()
-    pr.clear_background(pr.WHITE)
- 
-    mousePoint = pr.get_mouse_position()
+    while not pr.window_should_close():
+        pr.begin_drawing()
+        pr.clear_background(pr.WHITE)
+    
+        mousePoint = pr.get_mouse_position()
 
-    for s in ir_map.States:
-        s.draw()
+        for s in ir_map.States:
+            s.draw()
 
-    pr.end_drawing()
+        pr.end_drawing()
