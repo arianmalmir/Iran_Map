0f3976457f1506ae836b54be3f058b71c395426d
sharifigiv <76777409+sharifigiv@users.noreply.github.com>
2023-03-25 11:24:23 +0330

    Remove Button Class

--- Transform.py
+++ Transform.py
@@ -2,13 +2,14 @@ from pyray import (
     load_image, unload_image, 
     load_texture_from_image, 
     draw_texture, Vector2,
-    WHITE , BLACK, draw_rectangle,
+    WHITE , BLACK, GREEN, RED, ORANGE, YELLOW, PINK,
+    draw_rectangle,
     )
 
 class State:
     def __init__(self, x, y, img):
-        self.x = x
-        self.y = y
+        self.x = x 
+        self.y = y 
 
         img = load_image(img)
 
@@ -18,19 +19,3 @@ class State:
 
     def draw(self):
         draw_texture(self.tx, self.x, self.y, WHITE)
-
-class Button:
-    def __init__(self , pos : Vector2 , width : int , height : int, color = BLACK):
-
-        self.pos = pos
-        self.width = width
-        self.height = height
-        self.color = color
-
-        draw_rectangle(int(self.pos.x), int(self.pos.y), width, height, color)
-    
-    def exit(self):
-        pass
-    
-    def next(self):
-        pass
